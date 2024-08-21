7af0d8f53e903e16ae5fd857944d3c64b80edf0b
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-08-20 22:25:08 -0600

    feat(db): create schemas for all required tables
    BREAKING CHANGE: add all the required schemas to manage the platform

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,36 +1,232 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
-  index,
-  pgTableCreator,
-  serial,
+  boolean,
+  integer,
+  jsonb,
+  pgEnum,
+  pgTable,
+  primaryKey,
   timestamp,
+  uniqueIndex,
+  uuid,
   varchar,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `advanced-file-management_${name}`);
+export const permissionLevelsEnum = pgEnum("workspace_permission_levels_enum", [
+  "read",
+  "write",
+  "admin",
+]);
 
-export const posts = createTable(
-  "post",
+export const workspaces = pgTable(
+  "workspace",
   {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
+    id: uuid("id").defaultRandom().primaryKey(),
+    ownerId: varchar("owner_id", { length: 255 }).notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    isShared: boolean("is_shared").default(false).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at")
+      .defaultNow()
+      .$onUpdateFn(() => sql`current_timestamp`)
       .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (ws) => ({
+    ownerWorkspaceUnique: uniqueIndex("owner_workspace_unique").on(
+      ws.ownerId,
+      ws.name,
+    ),
+  }),
 );
+
+export const workspacesRelations = relations(workspaces, ({ many }) => ({
+  collaborators: many(workspaceCollaborators),
+  folders: many(folders),
+}));
+
+export const workspaceCollaborators = pgTable("workspace_collaborator", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  workspaceId: uuid("workspace_id")
+    .notNull()
+    .references(() => workspaces.id),
+  userId: varchar("user_id", { length: 255 }).notNull(),
+  permissionLevel: permissionLevelsEnum("permission_level").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdateFn(() => sql`current_timestamp`)
+    .notNull(),
+});
+
+export const workspaceCollaboratorsRelations = relations(
+  workspaceCollaborators,
+  ({ one }) => ({
+    workspace: one(workspaces, {
+      fields: [workspaceCollaborators.id],
+      references: [workspaces.id],
+    }),
+  }),
+);
+
+export const folders = pgTable("folder", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  workspaceId: uuid("workspace_id").notNull(),
+  parentFolderId: uuid("parent_folder_id").references(() => workspaces.id),
+  owner_id: varchar("owner_id", { length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdateFn(() => sql`current_timestamp`)
+    .notNull(),
+});
+
+export const foldersRelations = relations(folders, ({ one, many }) => ({
+  workspace: one(workspaces, {
+    fields: [folders.workspaceId],
+    references: [workspaces.id],
+  }),
+  parentFolder: one(folders, {
+    fields: [folders.parentFolderId],
+    references: [folders.id],
+  }),
+  files: many(files),
+  collaborators: many(collaborators),
+}));
+
+export const files = pgTable("file", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  folderId: uuid("folder_id")
+    .notNull()
+    .references(() => folders.id),
+  owner_id: varchar("owner_id", { length: 255 }).notNull(),
+  content: jsonb("content").notNull(),
+  size: integer("size").notNull(),
+  currentVersionId: uuid("current_version_id"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdateFn(() => sql`current_timestamp`)
+    .notNull(),
+});
+
+export const filesRelations = relations(files, ({ one, many }) => ({
+  folder: one(folders, {
+    fields: [files.folderId],
+    references: [folders.id],
+  }),
+  currentVersion: one(fileVersions, {
+    fields: [files.currentVersionId],
+    references: [fileVersions.id],
+  }),
+  versions: many(fileVersions),
+  fileTags: many(fileTags),
+  collaborators: many(collaborators),
+}));
+
+export const fileVersions = pgTable("file_version", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  fileId: uuid("file_id")
+    .notNull()
+    .references(() => files.id),
+  versionNumber: integer("version_number").notNull(),
+  content: jsonb("content").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdateFn(() => sql`current_timestamp`)
+    .notNull(),
+});
+
+export const fileVersionsRelations = relations(fileVersions, ({ one }) => ({
+  file: one(files, {
+    fields: [fileVersions.fileId],
+    references: [files.id],
+  }),
+}));
+
+export const tags = pgTable(
+  "tag",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    workspaceId: uuid("workspace_id").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at")
+      .defaultNow()
+      .$onUpdateFn(() => sql`current_timestamp`)
+      .notNull(),
+  },
+  (tags) => ({
+    workspaceTagUnique: uniqueIndex("workspace_tag_unique").on(
+      tags.workspaceId,
+      tags.name,
+    ),
+  }),
+);
+
+export const tagsRelations = relations(tags, ({ one, many }) => ({
+  workspace: one(workspaces, {
+    fields: [tags.workspaceId],
+    references: [workspaces.id],
+  }),
+  fileTags: many(fileTags),
+}));
+
+export const fileTags = pgTable(
+  "file_tag",
+  {
+    fileId: uuid("file_id")
+      .notNull()
+      .references(() => files.id),
+    tagId: uuid("tag_id")
+      .notNull()
+      .references(() => tags.id),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at")
+      .defaultNow()
+      .$onUpdateFn(() => sql`current_timestamp`)
+      .notNull(),
+  },
+  (fileTags) => ({
+    pk: primaryKey({ columns: [fileTags.fileId, fileTags.tagId] }),
+  }),
+);
+
+export const fileTagsRelations = relations(fileTags, ({ one }) => ({
+  file: one(files, {
+    fields: [fileTags.fileId],
+    references: [files.id],
+  }),
+  tag: one(tags, {
+    fields: [fileTags.tagId],
+    references: [tags.id],
+  }),
+}));
+
+export const resourceTypeEnum = pgEnum("resource_type", ["folder", "file"]);
+
+export const collaborators = pgTable("collaborator", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  userId: varchar("user_id", { length: 255 }).notNull(),
+  resourceType: resourceTypeEnum("resource_type").notNull(),
+  resourceId: uuid("resource_id").notNull(),
+  permissionLevel: permissionLevelsEnum("permission_level").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdateFn(() => sql`current_timestamp`)
+    .notNull(),
+});
+
+export const collaboratorsRelations = relations(collaborators, ({ one }) => ({
+  folder: one(folders, {
+    fields: [collaborators.resourceId],
+    references: [folders.id],
+  }),
+  file: one(files, {
+    fields: [collaborators.resourceId],
+    references: [files.id],
+  }),
+}));
