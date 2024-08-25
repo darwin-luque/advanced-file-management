7e719a3df0d1c14601c037e2eb33a55356146a8a
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-08-25 12:25:32 -0600

    chore: add multiple new ui components

--- src/constants/cookies.ts
+++ src/constants/cookies.ts
@@ -1,7 +1,3 @@
 export const COOKIE_PREFIX = "advanced-file-manager_";
 
-export const COOKIE_APP_RESIZABLE_PANELS = `${COOKIE_PREFIX}app-layout-resizable-panels`;
-export const COOKIE_APP_RESIZABLE_PANELS_LAYOUT = `${COOKIE_APP_RESIZABLE_PANELS}:layout`;
-export const COOKIE_APP_RESIZABLE_PANELS_COLLAPSED = `${COOKIE_APP_RESIZABLE_PANELS}:collapsed`;
-
 export const COOKIE_CURRENT_WORKSPACE_ID = `${COOKIE_PREFIX}current-workspace-id`;
