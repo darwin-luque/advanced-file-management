ee02716a7bcb9bc55501957cc34707c94428ace2
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-08-26 17:27:57 -0600

    chore: create files router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,12 @@
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { workspacesRouter } from "./routers/workspaces";
 import { foldersRouter } from "./routers/folders";
+import { filesRouter } from "./routers/files";
 
 export const appRouter = createTRPCRouter({
   workspaces: workspacesRouter,
   folders: foldersRouter,
+  files: filesRouter,
 });
 
 export type AppRouter = typeof appRouter;
