f9b1f32cfaf817610e3a877df94c5eec15ac702f
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-08-26 17:28:09 -0600

    chore: initial tiptap setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+const path = await import('path');
+
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias.yjs = path.resolve(
+        path.dirname(process.cwd()),
+        'node_modules/yjs')
+    }
+    return config
+  },
+};
 
 export default config;
