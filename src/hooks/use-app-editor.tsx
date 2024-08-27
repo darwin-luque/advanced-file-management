d070a9bd35ca04ee7be3b5a1e1447249c959d867
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-08-26 22:35:51 -0600

    refactor: improve styling in lists and headings

--- src/hooks/use-app-editor.tsx
+++ src/hooks/use-app-editor.tsx
@@ -4,12 +4,16 @@ import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Focus from "@tiptap/extension-focus";
 import Image from "@tiptap/extension-image";
+import Heading from "@tiptap/extension-heading";
+import ListItem from "@tiptap/extension-list-item";
 import TaskList from "@tiptap/extension-task-list";
 import TaskItem from "@tiptap/extension-task-item";
 import Paragraph from "@tiptap/extension-paragraph";
 import Underline from "@tiptap/extension-underline";
 import Highlight from "@tiptap/extension-highlight";
 import TextAlign from "@tiptap/extension-text-align";
+import BulletList from "@tiptap/extension-bullet-list";
+import OrderedList from "@tiptap/extension-ordered-list";
 import CharacterCount from "@tiptap/extension-character-count";
 
 type UseAppEditorProps = {
@@ -19,25 +23,48 @@ type UseAppEditorProps = {
 export const useAppEditor = ({ content }: UseAppEditorProps = {}) => {
   const editor = useEditor({
     extensions: [
+      Image,
       TaskList,
       TaskItem,
-      Highlight,
+      ListItem,
+      Underline,
       TextAlign,
       StarterKit,
-      Image,
+      CharacterCount,
+      Heading.configure({
+        levels: [1, 2, 3, 4, 5, 6],
+        HTMLAttributes: {
+          class: "leading-5 mt-10 text-pretty font-bold",
+        },
+      }),
+      Highlight.configure({
+        HTMLAttributes: {
+          class: "bg-yellow-300",
+        },
+      }),
+      Focus.configure({
+        className: "outline-none",
+      }),
+      BulletList.configure({
+        HTMLAttributes: {
+          class: "list-disc ml-6 space-y-2",
+        },
+      }),
+      OrderedList.configure({
+        HTMLAttributes: {
+          class: "list-decimal ml-6 space-y-2",
+        },
+      }),
       Paragraph.configure({
         HTMLAttributes: {
           class: "w-full",
         },
       }),
-      Underline,
-      CharacterCount,
-      Focus,
     ],
     editorProps: {
       attributes: {
         class:
-          "prose dark:prose-invert prose-base flex-1 focus:outline-none",
+          "prose dark:prose-invert prose-base flex-1 focus:outline-none space-y-2",
       },
     },
     content:
